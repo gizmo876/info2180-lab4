c1dd8c36670bc9a3d6c3db44ec2607a9aaaf190c
Shakielle Powell <shakielle.powell@mymona.uwi.edu>
2016-10-15 01:42:14 +0000

    Complete first exercise

---
+++ maze.js
@@ -0,0 +1,8 @@
+
+window.onload = function(){
+    document.getElementById("boundary1").addEventListener("mouseover",borderHover);
+}
+
+function borderHover(){
+    document.getElementById("boundary1").className = " boundary youlose";
+}
